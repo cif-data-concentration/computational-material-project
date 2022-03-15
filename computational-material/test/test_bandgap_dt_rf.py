6a4bd5c9021a00d041844a4b13a1027ce933964b
Yyhan-1998 <83165844+Yyhan-1998@users.noreply.github.com>
2022-03-14 20:45:01 -0700

    Update test_bandgap_dt_rf.py

--- computational-material/test/test_bandgap_dt_rf.py
+++ computational-material/test/test_bandgap_dt_rf.py
@@ -5,7 +5,53 @@ import sklearn
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-import bandgap_dt_rf
+
+df = pd.read_csv('../../doc/dataset/bg_struct.csv', index_col=0)
+X = df[['cell_length_a','cell_lentgh_b', 'cell_lentgh_c',
+        'cell_angle_alpha', 'cell_angle_beta', 'cell_angle_gamma']]
+y = df['bandgap']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20,
+                                                    random_state=42)
+
+
+# Define the decision tree regressor object
+regr = DecisionTreeRegressor(max_depth=9, min_samples_split=37, min_samples_leaf=4,
+                  splitter='best', min_weight_fraction_leaf=0.00952179564895864)
+
+#Fit to the training data
+regr = regr.fit(X_train, y_train)
+
+# Define a random forest regressor object
+rand = RandomForestRegressor(n_estimators=99, max_depth=23, min_samples_split=14,
+            min_samples_leaf=3, min_weight_fraction_leaf=2.9893745932052474e-05)
+
+# Fit to your training data
+rand = rand.fit(X_train, y_train)
+
+def bandgap_dt(a, b, c, alpha, beta, gamma):
+    '''
+    This function is to use the training decision tree regressor model to
+    predict the bandgap value.
+    '''
+    test = [a, b, c, alpha, beta, gamma]
+    # convert the list to the dataframe
+    test_point = pd.DataFrame([test], columns=['cell_length_a','cell_lentgh_b', 'cell_lentgh_c',
+                                             'cell_angle_alpha', 'cell_angle_beta', 'cell_angle_gamma'])
+    return regr.predict(test_point)[0]
+
+
+def bandgap_rf(a, b, c, alpha, beta, gamma):
+    '''
+    This function is to use the training decision tree regressor model to
+    predict the bandgap value.
+    '''
+    test = [a, b, c, alpha, beta, gamma]
+    # convert the list to the dataframe
+    test_point = pd.DataFrame([test], columns=['cell_length_a','cell_lentgh_b', 'cell_lentgh_c',
+                                             'cell_angle_alpha', 'cell_angle_beta', 'cell_angle_gamma'])
+    return rand.predict(test_point)[0]
+
+
 
 class UnitTests(unittest.TestCase):
     def test_bandgap_dt(self):
